e8e7330546aff057099948eda3af8d20e9011705
Kitee666 <ghost2rider99@gmail.com>
2021-12-01 04:23:12 +0100

    fresh start

---
+++ src/test/java/pl/umk/mat/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umk.mat.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

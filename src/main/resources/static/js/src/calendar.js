427f351efaa4f231ae070708f1e0fbb691ed323f
Dominik Sobolewski <domin25001@gmail.com>
2022-03-21 19:55:51 +0100

    Rozpoczęcie pracy nad głównym widokiem kalendarza
    Zmiany w głównym widoku:
    - Polska wersja językowa
    - Określenie trybu weekendowego
    
    Zmiany wizualne:
    - Nagłówek
    
    Zmiany w strukturze projektu:
    - Ogarnięcie plików, podzielenie ich odpowiednio na .js, .css
    - Małe porzadki w kodzie

---
+++ src/main/resources/static/js/src/calendar.js
@@ -0,0 +1,75 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var Calendar = FullCalendar.Calendar;
+    var Draggable = FullCalendar.Draggable;
+
+    var containerEl = document.getElementById('external-events');
+    var calendarEl = document.getElementById('calendar');
+    var checkbox = document.getElementById('drop-remove');
+
+    // initialize the external events
+    // -----------------------------------------------------------------
+
+    new Draggable(containerEl, {
+        itemSelector: '.fc-event',
+        eventData: function(eventEl) {
+            return {
+                title: eventEl.innerText,
+
+
+
+            };
+
+        },
+    });
+
+    // initialize the calendar
+    // -----------------------------------------------------------------
+
+
+    var calendar = new Calendar(calendarEl, {
+        locale: 'pl',
+        headerToolbar: {
+            left: 'prev,next,today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridTwoDays',
+
+        },
+        views: {
+            timeGridTwoDays: {
+                type: 'timeGrid',
+                duration: { days: 7 },
+                buttonText: 'Weekendy',
+                hiddenDays: [ 1, 2, 3, 4, 5],
+                weekday: 'long',
+                // slotDuration: '01:00:00'
+            },
+            dayGridMonth: {
+                buttonText: 'Widok miesiąca'
+            },
+        },
+    buttonText: {
+            today: 'Dzisiaj'
+    },
+
+        events: [
+            {
+                id: '.fc-event',
+
+            }
+        ],
+
+        editable: true,
+        droppable: true, // this allows things to be dropped onto the calendar
+        drop: function(info) {
+            // is the "remove after drop" checkbox checked?
+            if (checkbox.checked) {
+                // if so, remove the element from the "Draggable Events" list
+                info.draggedEl.parentNode.removeChild(info.draggedEl);
+            }
+        }
+    });
+
+    calendar.render();
+});

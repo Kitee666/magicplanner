a3b726de7035e49897d850ae06a526b01b60e275
Kitee666 <18664027+Kitee666@users.noreply.github.com>
2022-04-06 23:00:51 +0200

    api and security update

---
+++ src/main/java/pl/umk/mat/planner/SecurityConfig.java
@@ -0,0 +1,18 @@
+package pl.umk.mat.planner;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers(POST, "/api/lecturers").permitAll();
+    }
+}
--- src/main/java/pl/umk/mat/planner/lecturer/LecturerController.java
+++ src/main/java/pl/umk/mat/planner/lecturer/LecturerController.java
@@ -1,6 +1,8 @@
 package pl.umk.mat.planner.lecturer;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,8 +16,13 @@ public class LecturerController {
         this.repository = repository;
     }
 
-    @GetMapping("/lecturers")
+    @GetMapping("/api/lecturers")
     List<Lecturer> all() {
         return repository.findAll();
+    }
+
+    @PostMapping("/api/lecturers")
+    Lecturer newLecturer(@RequestBody Lecturer newLecturer) {
+        return repository.save(newLecturer);
     }
 }

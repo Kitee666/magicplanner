66acfea8cf0cceb03e2e39b51462f75dafb53455
Kitee666 <18664027+Kitee666@users.noreply.github.com>
2022-04-07 16:48:26 +0200

    security temporary auth

--- src/main/java/pl/umk/mat/planner/security/SecurityConfig.java
+++ src/main/java/pl/umk/mat/planner/security/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","index","/css/*","/js/*")
+                .antMatchers("/","/admin","index","/css/*","/js/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()

a6c75ee2da9677e02d54f4d6438151d5e13d9d02
Kitee666 <18664027+Kitee666@users.noreply.github.com>
2022-04-07 17:47:22 +0200

    csrf tmp

--- src/main/java/pl/umk/mat/planner/security/SecurityConfig.java
+++ src/main/java/pl/umk/mat/planner/security/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/","/admin","index","/css/*","/js/*")
                 .permitAll()

132e671a2e00654e8dc10796e4b20a11355c353e
Kitee666 <18664027+Kitee666@users.noreply.github.com>
2022-05-28 01:20:55 +0200

    connector and js fixes

--- src/main/java/pl/umk/mat/planner/event/EventRepository.java
+++ src/main/java/pl/umk/mat/planner/event/EventRepository.java
@@ -3,24 +3,24 @@ package pl.umk.mat.planner.event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.lang.NonNull;
-import pl.umk.mat.planner.mappers.EventInfo;
+import pl.umk.mat.planner.types.groupType;
+import pl.umk.mat.planner.types.yearType;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-//    @Query("SELECT e FROM Event e left join fetch e.connector")
-//    List<EventInfo> findBetween();
+    @Query("select e from Event e where e.connector.subject.year = ?1")
+    Set<Event> findByConnector_Subject_Year(yearType year);
 
-//    @Query("""
-//            select e from Event e
-//            left join e.connector c
-//            where e.dateFrom between :dateStart and :dateEnd or e.dateTo between :dateStart and :dateEnd""")
-//    List<EventInfo> findByDateBetween(@Param("dateFromStart") @NonNull OffsetDateTime dateStart, @Param("dateFromEnd") @NonNull OffsetDateTime dateEnd);
-    @Query("""
-            select e from Event e
-            left join e.connector c
-            """)
-    List<EventInfo> findByDateBetween();
+    @Override
+    Optional<Event> findById(Long aLong);
+
+//    Set<Event> findByDateFromIsBetweenAndDateToIsBetweenAndConnector_Subject_YearIs(OffsetDateTime dateFromStart, OffsetDateTime dateFromEnd, OffsetDateTime dateToStart, OffsetDateTime dateToEnd, yearType year);
+
+
 }
